46c384c161312ffe3f5c42cd2b4977a5de82641e
Fernando Octavio Sanchez Chavez <miloscorpio15@gmail.com>
2019-04-06 14:56:57 -0600

    actualizacionde productos

--- server/server.js
+++ server/server.js
@@ -7,8 +7,14 @@ require('dotenv').config();
 
 //modelos a importar 
 const  { User } = require('./models/user');
+const { Brand } = require('./models/brand');
+const { Wood } = require('./models/wood');
+const { Product } = require('./models/product');
+
 // constante para utilizar autenticacion siempre 
-const { auth } = require('./middleware/auth')
+const { auth } = require('./middleware/auth');
+const { admin } = require('./middleware/admin');
+
 
 //Crear cadena de conexion 
 mongoose.connect(process.env.DATABASE, {useNewUrlParser:true, 'useCreateIndex': true}, (err) => {
@@ -87,4 +93,76 @@ app.get('/api/user/logout', auth, (req, res) => {
           })
       }
   )
+})
+
+//ruta Brand () crear guitarra 
+app.post('/api/product/brand', auth, admin, (req, res) => {
+  const brand = new Brand(req.body)
+  brand.save((err, doc) => {
+      if(err) return res.json({success: false, err})
+      res.status(200).json({
+          success: true,
+          brand: doc
+      })
+  })
+})
+
+//Consultar marcas de Guitarras
+app.get('/api/product/brands', (req, res) => {
+  Brand.find({}, (err, brands) => { 
+      if(err) return res.status(400).send(err)
+      res.status(200).send(brands)
+  })
+})
+
+// Dar de alta WOOD 
+app.post('/api/product/wood', auth, admin, (req,res)=>{
+  const wood = new Wood(req.body)
+  wood.save((err,doc) => {
+      if(err) return res.json({success: false, err})
+      res.status(200).json({
+          success: true,
+          wood: doc
+      })
+  })
+})
+
+app.get('/api/product/woods', (req, res) => {
+  Wood.find({}, (err, woods) => {
+      if(err) return res.status(400).send(err)
+      res.status(200).send(woods)
+  }) 
+})
+
+/* Crear alta de producrto */
+app.post('/api/product/article', auth, admin, (req, res) => {
+  const product = new Product(req.body)
+  product.save((err, doc) => {
+      if(err) return res.json({success: false, err})
+      res.status(200).json({
+          success: true,
+          article: doc
+      })
+  })
+})
+/* Consulta de productod By Id */
+app.get('/api/product/articles_by_id',( req, res ) => {
+  let type = req.query.type
+  let items = req.query.id
+  
+  if(type === "array"){
+      let ids = req.query.id.split(',')
+      items = []
+      items = ids.map(item => { 
+          // Convertirlos en ObjectId de Mongoose
+          return mongoose.Types.ObjectId(item)
+      })
+  }
+  Product.
+  find({ '_id': {$in:items}})
+  .populate('brand')
+  .populate('wood')
+  .exec((err, docs)=> {
+      return res.status(200).send(docs)
+  })
 })
